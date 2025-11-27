6ebcffb37e0a9e725dfe7490245428a7e7726613
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-11-27 23:55:10 +0530

    cloudflare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     devtools(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
